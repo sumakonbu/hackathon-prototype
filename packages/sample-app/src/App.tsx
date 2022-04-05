eb4d74a1c26f8c3440892586f9867d22df019f81
T2 <studioTeaTwo@gmail.com>
2022-04-05 15:50:07 +0900

    fix(sample-app): implement eslint for workspace

--- packages/sample-app/src/App.tsx
+++ packages/sample-app/src/App.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Awesome App</h1>
-        <p>
-          Now APY 100%!
-        </p>
+        <p>Now APY 100%!</p>
       </header>
     </div>
   );
