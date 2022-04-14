69d484bbd79db444ec99c3d387729051e4875cbd
T2 <studioTeaTwo@gmail.com>
2022-04-14 14:44:13 +0900

    fix

--- packages/console-app/src/app/persons/register/register.component.ts
+++ packages/console-app/src/app/persons/register/register.component.ts
@@ -56,9 +56,9 @@ export class RegisterComponent {
 
   private addressValidator(control: AbstractControl) {
     // disable to make debug easy with TESTNET.
-    if (this.metamaskService.currentNetwork !== '336') {
-      return null;
-    }
+    // if (this.metamaskService.currentNetwork !== '336') {
+    //   return null;
+    // }
 
     const isAddress = ethers.utils.isAddress(control.value);
     return !isAddress ? { invalidAddress: { value: control.value } } : null;
